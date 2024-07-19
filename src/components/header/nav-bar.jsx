d9dd2f52bd21fc9995dbead99dab04fab04bbac1
Badrú <sbsbadroo@gmail.com>
2024-07-19 16:51:31 +0200

    Logo

--- src/components/header/nav-bar.jsx
+++ src/components/header/nav-bar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 
-const activeStyles = "text-blue-500";
-const inactiveStyles = "text-white";
+const activeStyles = "text-blue-500 hover:text-blue-400 transition-all duration-500 ease-in-out";
+const inactiveStyles = "text-white hover:text-blue-400 transition-all duration-500 ease-in-out";
 export const NavBar = () => {
   return (
     <nav className="space-x-10 text-lg ">
